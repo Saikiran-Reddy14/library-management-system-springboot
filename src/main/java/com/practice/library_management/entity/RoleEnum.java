2e24b27fb7cef66f0e897f8175cbd65e905369d1
Saikiran-Reddy14 <saikian.kadari14@gmail.com>
2026-03-04 21:29:32 +0530

    Add RoleEnum and User entity classes for user management

---
+++ src/main/java/com/practice/library_management/entity/RoleEnum.java
@@ -0,0 +1,6 @@
+package com.practice.library_management.entity;
+
+public enum RoleEnum {
+    ADMIN,
+    USER
+}
---
+++ src/main/java/com/practice/library_management/entity/User.java
@@ -0,0 +1,37 @@
+package com.practice.library_management.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    private String name;
+
+    private String email;
+
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private RoleEnum role;
+}
