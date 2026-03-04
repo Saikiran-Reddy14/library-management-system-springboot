c4be70cd332ac33904c0cf7e9836aee2df61c9fd
Saikiran-Reddy14 <saikian.kadari14@gmail.com>
2026-03-05 00:10:29 +0530

    Add JwtUtils class for JWT token generation and validation

---
+++ src/main/java/com/practice/library_management/config/JwtUtils.java
@@ -0,0 +1,73 @@
+package com.practice.library_management.config;
+
+import java.security.Key;
+import java.util.Date;
+
+import javax.crypto.SecretKey;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+
+@Component
+public class JwtUtils {
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    @Value("${jwt.accessTokenExpiration}")
+    private String accessTokenExpiration;
+
+    @Value("${jwt.refreshTokenExpiration}")
+    private String refreshTokenExpiration;
+
+    public SecretKey getSignKey() {
+        return Keys.hmacShaKeyFor(secretKey.getBytes());
+    }
+
+    private String buildToken(String email, long expiration) {
+        return Jwts.builder()
+                .subject(email)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(getSignKey())
+                .compact();
+    }
+
+    public String generateAccessToken(String email) {
+        return buildToken(email, Long.parseLong(accessTokenExpiration));
+    }
+
+    public String generateRefreshToken(String email) {
+        return buildToken(email, Long.parseLong(refreshTokenExpiration));
+    }
+
+    public boolean isTokenValid(String token, String email) {
+        try {
+            String extractedEmail = extractEmail(token);
+            return email.equals(extractedEmail) && !isTokenExpired(token);
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public boolean isTokenExpired(String token) {
+        try {
+            return extractClaims(token).getExpiration().before(new Date());
+        } catch (Exception e) {
+            return true;
+        }
+    }
+
+    public String extractEmail(String token) {
+        return extractClaims(token).getSubject();
+    }
+
+    public Claims extractClaims(String token) {
+        return Jwts.parser().verifyWith(getSignKey()).build().parseSignedClaims(token).getPayload();
+    }
+
+}
