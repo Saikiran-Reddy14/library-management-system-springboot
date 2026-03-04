13a813525d890239e0572e716b8b48a96735dad2
Saikiran-Reddy14 <saikian.kadari14@gmail.com>
2026-03-05 00:08:39 +0530

    Add CustomUserDetails and CustomUserDetailsService for user authentication

---
+++ src/main/java/com/practice/library_management/utils/CustomUserDetailsService.java
@@ -0,0 +1,25 @@
+package com.practice.library_management.utils;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.practice.library_management.repo.UserRepo;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepo userRepo;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepo.findByEmail(email)
+                .map(user -> new CustomUserDetails(user))
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
+    }
+
+}
