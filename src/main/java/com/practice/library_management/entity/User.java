98adafd2cdae108f685e4d8f9b2c377c489fe631
Saikiran-Reddy14 <saikian.kadari14@gmail.com>
2026-03-07 17:56:40 +0530

    Add borrowRecords relationship to User entity

--- src/main/java/com/practice/library_management/entity/User.java
+++ src/main/java/com/practice/library_management/entity/User.java
@@ -45,4 +45,7 @@ public class User {
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<RefreshToken> refreshTokens;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<BorrowRecord> borrowRecords;
 }
